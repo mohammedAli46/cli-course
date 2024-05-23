17697caa6764b1248cf1c292960f344a95ff06c6
mohammedAli46 <94938119+mohammedAli46@users.noreply.github.com>
2024-05-23 10:36:10 +0000

    Added Operators & Type Conversion file

--- courses/javascript/task/ali.js
+++ courses/javascript/task/ali.js
@@ -9,4 +9,65 @@ var broughtUp = "Hyderabad";
 
 var out = `Hello EveryOne, My name is ${firstName} ${middleName} ${lastName}, I Born in ${born} and Brought Up in ${broughtUp} and working as ${designation} at ${company}`
 
-console.log(out);
+//console.log(out);
+
+/* Operators Task*/
+
+a = 10;
+b = 20;
+
+/* Arthematic Operators */
+c = a+b;
+d = a-b;
+e = a*b;
+f = a/b;
+g = a%b;
+h = a**b
+i = ++a;
+j = --a;
+k = --a;
+
+/*Assignment Operators*/
+
+a1 = 5;
+//a2 = 5;
+
+a1 += 5;
+a1 -= 1;
+a1 *= 5;
+a1 /= 25;
+a1 %= 135;
+//console.log(a1);
+
+
+//Comparision Operators
+
+q = 5 == 5; //true
+q1 = 5 == '5' //true
+q2 =5 === 5;  //true
+q3 =5 === '5';  //false
+//console.log(q3);
+
+w1 = 2 > 4; //false
+w2 = 1 < 5; //true
+w3 = 1 <= 1; //true
+w4 = 1 <= 5; //true
+w5 = 5 <= 1; //false
+w6 = 4 >= 5; //false
+//w7 = 10 = 10; //no such comparision will get error.
+//console.log(w7);
+
+
+// TypeOf Conversion
+
+var price = 100; //number
+var price1 ='100'; //string
+//console.log(price1, typeof price1);
+
+var material = "table"; //table string
+var material1 = 100;  //100 number
+//var material3 = 200; //undefined undefined
+material1 = Boolean.material1; 
+
+//console.log(material1, typeof material1);
+
